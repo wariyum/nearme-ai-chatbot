4d3a977cbfe192cd5d14f84b73196127c862cb9e
harikrishnan2193 <harikrishnank2193@gmail.com>
2025-08-16 13:36:49 +0530

    chatbot ui commit initially

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import 'zone.js';
+import { createApplication } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
+import { ChatbotWidgetComponent } from './app/chatbot-widget.component';
+import { provideHttpClient } from '@angular/common/http';
+
+(async () => {
+  const app = await createApplication({
+    providers: [provideHttpClient()],
+  });
+
+  const el = createCustomElement(ChatbotWidgetComponent, { injector: app.injector });
+  customElements.define('cb-chatbot', el);
+})();
